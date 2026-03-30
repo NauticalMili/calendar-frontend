72f50a0167cf59e401fb2b8169eb9787ae489ce8
Milind Nautiyal <146779035+NauticalMili@users.noreply.github.com>
2026-03-30 12:31:24 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8080",
+        target: "https://application-elh-calender.19zsvha555os.eu-gb.codeengine.appdomain.cloud",
         changeOrigin: true,
       },
     },
